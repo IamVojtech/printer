d7c3cb22e2736c0015fb5243a98c142ce093d739
IamVojtech <iamvojtech@gmail.com>
2025-02-10 20:12:26 +0100

    Added text input

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,11 @@ mod chars;
 mod writer;
 
 fn main() {
-    let mut printer = Printer::init().unwrap();
+    let mut input = String::new();
+    print!("Zadej text k vytištění: ");
+    let b1 = std::io::stdin().read_line(&mut input).unwrap();
+
+    let mut printer = Printer::init(input).unwrap();
 
     printer.start_drawing();
 }
